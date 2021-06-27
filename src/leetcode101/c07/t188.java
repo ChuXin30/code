a7543e7922f64dd157edc1accc0d170e087edacc
hao <957652322@qq.com>
2021-06-27 22:27:45 +0800

    宿舍 leetcode101 c07 动态规划 t4

--- src/leetcode101/c07/t188.java
+++ src/leetcode101/c07/t188.java
@@ -1,10 +1,8 @@
 package leetcode101.c07;
-<<<<<<< HEAD
 
 import java.util.Arrays;
 
-=======
->>>>>>> origin/master
+
 //188. 买卖股票的最佳时机 IV
 //        给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
 //
@@ -26,36 +24,6 @@ import java.util.Arrays;
 //        解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
 //        随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
 
-<<<<<<< HEAD
-public class t188 {
-    class Solution {
-        public int maxProfit(int k, int[] prices) {
-            if (prices.length == 0) {
-                return 0;
-            }
-
-            int n = prices.length;
-            k = Math.min(k, n / 2);
-            int[][] buy = new int[n][k + 1];
-            int[][] sell = new int[n][k + 1];
-
-            buy[0][0] = -prices[0];
-            sell[0][0] = 0;
-            for (int i = 1; i <= k; ++i) {
-                buy[0][i] = sell[0][i] = Integer.MIN_VALUE / 2;
-            }
-
-            for (int i = 1; i < n; ++i) {
-                buy[i][0] = Math.max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
-                for (int j = 1; j <= k; ++j) {
-                    buy[i][j] = Math.max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
-                    sell[i][j] = Math.max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
-                }
-            }
-
-            return Arrays.stream(sell[n - 1]).max().getAsInt();
-        }
-=======
 /*
 使用动态规划来做
 使用两个数组一个为buy， buy[i] 标识第i次 买入股票的最大收入
@@ -88,6 +56,5 @@ public class t188 {
             maxProfit += prices[i] - prices[i-1];
         }
         return maxProfit;
->>>>>>> origin/master
     }
 }
