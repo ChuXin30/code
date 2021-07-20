66d52ecf5b1be6e856b9de685ca93471f722b9eb
hao <957652322@qq.com>
2021-07-20 14:00:21 +0800

    laptop no bug

--- src/coreJava/p05/inheritance/Manager.java
+++ src/coreJava/p05/inheritance/Manager.java
@@ -1,30 +1,32 @@
 package coreJava.p05.inheritance;
 
-public class Manager extends Employee{
-    private double bouns;
+import coreJava.p05.abstractClasses.Employee;
 
-    public Manager(String n , double s){
-        super(n , s);
-        bouns = 0;
-    }
-
-    @Override
-    public double getSalary() {
-        return bouns+super.getSalary();
-    }
-
-    @Override
-    public String toString() {
-        return "Manager{" +
-                "bouns=" + bouns + " salery"+getSalary()+
-                '}';
-    }
-
-    public double getBouns() {
-        return bouns;
-    }
-
-    public void setBouns(double bouns) {
-        this.bouns = bouns;
-    }
-}
+//public class Manager extends Employee {
+//    private double bouns;
+//
+//    public Manager(String n , double s){
+//        super(n , s);
+//        bouns = 0;
+//    }
+//
+//    @Override
+//    public double getSalary() {
+//        return bouns+super.getSalary();
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Manager{" +
+//                "bouns=" + bouns + " salery"+getSalary()+
+//                '}';
+//    }
+//
+//    public double getBouns() {
+//        return bouns;
+//    }
+//
+//    public void setBouns(double bouns) {
+//        this.bouns = bouns;
+//    }
+//}
