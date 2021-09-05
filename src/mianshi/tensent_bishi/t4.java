8a2feb4df9dd78f2bfaa14ab0ed21dfe63b61236
hao <957652322@qq.com>
2021-09-05 22:12:47 +0800

    实验室 腾讯9.05 笔试

---
+++ src/mianshi/tensent_bishi/t4.java
@@ -0,0 +1,12 @@
+package mianshi.tensent_bishi;
+
+import java.util.Scanner;
+
+public class t4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+
+        }
+    }
+}
---
+++ src/mianshi/tensent_bishi/t5.java
@@ -0,0 +1,12 @@
+package mianshi.tensent_bishi;
+
+import java.util.Scanner;
+
+public class t5 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner();
+//        while (scanner.hasNext()){
+//            //设置一个二维矩阵保存了 martix[i][j] 保存了i-j 区间的最小值
+//        }
+//    }
+}
